226aae0b8cd650f6b9811ca83bf16bfaabb5c325
Timbuli Remus <kapucin28@gmail.com>
2016-05-07 17:44:37 +0300

    Creating UI package

---
+++ src/rollMessContent/RollUI.java
@@ -0,0 +1,15 @@
+package rollMessContent;
+
+import javafx.scene.layout.Pane;
+
+/**
+ * Created by TIMBULI REMUS K@puc!n on 07-May-16.
+ */
+public class RollUI extends Pane {
+
+    // Constructor------------------------------------------------------------------------------------------------------
+    public RollUI() {
+
+    }
+    //------------------------------------------------------------------------------------------------------------------
+}
