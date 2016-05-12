93e7b1fc438683f3f7b2dfe2bf8e5c85f1d09fbf
Timbuli Remus <kapucin28@gmail.com>
2016-05-12 14:43:51 +0300

    Adding socket getter

--- src/alerts/ExitAlert.java
+++ src/alerts/ExitAlert.java
@@ -4,11 +4,18 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.stage.Modality;
 import javafx.stage.StageStyle;
+import rollMessContent.RollUI;
+
+import java.io.IOException;
 
 /**
  * Created by TIMBULI REMUS K@puc!n on 04-May-16.
  */
 public class ExitAlert {
+
+    // Getting the socket variable--------------------------------------------------------------------------------------
+    private RollUI rollUI = new RollUI();
+    //------------------------------------------------------------------------------------------------------------------
 
     // Alert variable---------------------------------------------------------------------------------------------------
     private Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Exit Program", ButtonType.OK, ButtonType.CANCEL);
@@ -27,6 +34,11 @@ public class ExitAlert {
         alert.initStyle(StageStyle.TRANSPARENT);
         alert.showAndWait();
         if (alert.getResult() == ButtonType.OK) {
+            try {
+                rollUI.getChatSocket().close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             System.exit(0);
         } else {
             alert.close();
