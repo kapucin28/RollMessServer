8fd9f2dab0b412726c45785948ffc1860c8df1b9
kapucin28 <kapucin28@gmail.com>
2016-12-01 01:57:33 +0200

    Implementing interface

---
+++ src/interfaces/ObjectTitles.java
@@ -0,0 +1,13 @@
+package interfaces;
+
+/**
+ * Created by Timbuli Remus on 01.12.2016.
+ * <p>
+ * Interface for objects
+ */
+public interface ObjectTitles {
+
+    String cssFile = "style.css";
+    String stageTitle = "RollMessServer";
+
+}
