b0beb6754b99c76470e4ffa6e4a21c04e17530da
kapucin28 <kapucin28@gmail.com>
2016-11-30 02:13:30 +0200

    Implementing interface

---
+++ src/interfaces/AlertsMessages.java
@@ -0,0 +1,13 @@
+package interfaces;
+
+/**
+ * Created by Timbuli Remus on 30.11.2016.
+ * <p>
+ * Interface for alerts
+ */
+public interface AlertsMessages {
+
+    String emptyMessage = "Empty Field";
+    String exitMessage = "Exit Program";
+
+}
