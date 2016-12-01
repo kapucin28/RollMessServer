1f01e2b8870ec90669e8ce290283a9b4a26e3c87
kapucin28 <kapucin28@gmail.com>
2016-12-02 01:26:24 +0200

    Implementing interface

--- src/interfaces/ObjectTitles.java
+++ src/interfaces/ObjectTitles.java
@@ -9,5 +9,18 @@ public interface ObjectTitles {
 
     String cssFile = "style.css";
     String stageTitle = "RollMessServer";
+    String fileMenu = "_File";
+    String editMenu = "_Edit";
+    String save = "Save As...";
+    String load = "Load...";
+    String exit = "Exit";
+    String clear = "Clear Text";
+    String textAreaPrompt = "Messages Received";
+    String textFieldPrompt = "Write message";
+    String textAreaAppend = "Server started at: ";
+    String textAreaAppendReceived = "Received from client: ";
+    String textAreaAppendSent = "Sent: ";
+    String extensionFilter = "All Files";
+    String extensionType = "*.*";
 
 }
